0537dc9b1d4d3df70091c6722c3f978aaba12296
Eldin Sahbaz <sahbaz.eldin@gmail.com>
2016-09-09 14:23:44 -0400

    IntPalindrome.java
    Given an int, return a boolean indicating whether or not the int is a palindrome.

---
+++ Algorithms/IntPalindrome.java
@@ -0,0 +1,23 @@
+/*
+Given an int, return a boolean indicating whether or not the int is a palindrome.
+*/
+
+public class IntPalindrome
+{
+  public boolean isPalindrome(int x)
+    {
+        int rebuild = 0;
+        int backUp = x;
+        x = Math.abs(x);
+        
+        while(x > 0)
+        {
+           int modded = x%10;
+           rebuild = (rebuild * 10) + modded;
+           x -= modded;
+           x /= 10;
+        }
+        
+        return (rebuild == backUp) ? true : false;
+    }
+}
