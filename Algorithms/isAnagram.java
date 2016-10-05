3df4ee6c8f9648181283fe8837c27284702ccda7
Eldin Sahbaz <sahbaz.eldin@gmail.com>
2016-10-04 22:40:57 -0400

    Update isAnagram.java

--- Algorithms/isAnagram.java
+++ Algorithms/isAnagram.java
@@ -21,6 +21,37 @@ public class isAnagram
         Arrays.sort(String1);
         Arrays.sort(String2);
         
-        return (String.valueOf(String1).intern() == String.valueOf(String2).intern());
+        return (String.valueOf(String1)/*.intern()*/ == String.valueOf(String2)/*.intern()*/);
   }
+  
+    //more efficient since sorting is only as fast as sorting method.
+    //Although this method is not as spatially efficient, it is more time
+    //efficient
+  	public static boolean isAnagramAlternate(String one, String two)
+	  {
+
+		  one = one.toLowerCase();
+		  two = two.toLowerCase();
+		  Map<Character, Integer> chars = new HashMap<>();
+		
+		  for(int i = 0; i < one.length(); i++)
+			  if(chars.get(one.charAt(i)) == null)
+				  chars.put(one.charAt(i), 1);
+			  else
+				  chars.put(one.charAt(i), chars.get(one.charAt(i)) + 1);
+
+		
+		  for(int i = 0; i < two.length(); i++)
+			  if(chars.get(two.charAt(i)) == null)
+				  return false;
+			  else
+			  {
+				  chars.put(two.charAt(i), chars.get(two.charAt(i)) - 1);
+
+				  if (chars.get(two.charAt(i)) == 0)
+					  chars.remove(two.charAt(i));
+			  }
+
+		  return chars.isEmpty();
+	  }
 }
