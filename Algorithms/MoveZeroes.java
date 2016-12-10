f1b9d0831f9d25857ee1f86851ad717ceed19863
Eldin Sahbaz <sahbaz.eldin@gmail.com>
2016-12-10 15:27:37 -0500

    Update MoveZeroes.java

--- Algorithms/MoveZeroes.java
+++ Algorithms/MoveZeroes.java
@@ -12,14 +12,13 @@ public class MoveZeroes
 {
   public static void moveZeroes(int[] nums)
   {
-    	int tracker = 0;
-    	for(int i = 0; i < nums.length; i++)
-    	{
-    		if(nums[i] != 0)
-    		{
-    			nums[tracker++] = nums[i];
-    			nums[i] = 0;
-    		}
-    	}
+    int tracker = 0;
+    	
+    for(int i = 0; i < nums.length; i++)
+      if(nums[i] != 0)
+        nums[tracker++] = nums[i];
+    	
+    while(tracker < nums.length)
+      nums[tracker++] = 0;
   }
 }
