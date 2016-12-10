2bcfb3ee03cc72f223e060e6db7f615c9db6545f
Eldin Sahbaz <sahbaz.eldin@gmail.com>
2016-12-10 12:53:30 -0500

    Create WeigherNumberGenerator.java

---
+++ Algorithms/WeigherNumberGenerator.java
@@ -0,0 +1,25 @@
+//weighted random number generator
+import java.util.Random;
+
+public static int WeightedGenerator(int[] weights)
+{
+  int sum = 0;
+  for(int i : weights)
+    sum += i;
+
+  Random rand = new Random();
+  int randomNumber = Math.abs(rand.nextInt()) % (sum+1);
+
+  for(int i = 0; i < weights.length; i++)
+  {
+    if(randomNumber - weights[i] > 0)
+      randomNumber -= weights[i];
+    else
+    {
+      randomNumber = i;
+      break;
+    }
+  }
+
+  return randomNumber;
+}
