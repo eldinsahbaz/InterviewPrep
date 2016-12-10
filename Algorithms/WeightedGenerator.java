b72624a3273c94f14b64ee44f6feb877d768d143
Eldin Sahbaz <sahbaz.eldin@gmail.com>
2016-12-10 12:54:42 -0500

    Update and rename WeigherNumberGenerator.java to WeightedGenerator.java

---
+++ Algorithms/WeightedGenerator.java
@@ -0,0 +1,28 @@
+//weighted random number generator
+import java.util.Random;
+
+class WeightedGenerator
+{
+  public static int generate(int[] weights)
+  {
+   int sum = 0;
+   for(int i : weights)
+      sum += i;
+
+    Random rand = new Random();
+    int randomNumber = Math.abs(rand.nextInt()) % (sum+1);
+
+    for(int i = 0; i < weights.length; i++)
+    {
+      if(randomNumber - weights[i] > 0)
+        randomNumber -= weights[i];
+      else
+      {
+        randomNumber = i;
+        break;
+      }
+    }
+
+    return randomNumber;
+  }
+}
